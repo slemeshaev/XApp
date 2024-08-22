91b79b862fe426c3e4002acfaa8ff482bd52f27a
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2024-08-22 08:18:21 +0300

    Created Navigation View

--- app/Content/sources/ContentView.swift
+++ app/Content/sources/ContentView.swift
@@ -10,9 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, how are you doing?")
-            .padding()
-        Text("I'm doing just fine")
+        NavigationView {
+            TabView {
+                Text("Feed View")
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                Text("Search View")
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+                Text("Message View")
+                    .tabItem {
+                        Image(systemName: "envelope")
+                        Text("Messages")
+                    }
+            }
+            .navigationBarTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
