bddd31507b4a44e687751e099b593eaa0d81804c
Stanislav Lemeshaev <lemeshaevs@gmail.com>
2024-09-03 07:40:19 +0300

    Created user profile view

---
+++ app/Content/sources/Profile/UserProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  UserProfileView.swift
+//  X
+//
+//  Created by Stanislav Lemeshaev on 03.09.2024.
+//  Copyright © 2024 slemeshaev. All rights reserved.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileView()
+}
